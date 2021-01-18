0200ca83ed1271f1b0fe9284d75bf3ba345e4a1c
cdamyx <chris@amyx.engineering>
2021-01-17 20:01:57 -0600

    first part of lesson 165 complete

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-var numberOfDrumButtons = document.querySelctorAll(".drum").length;
-
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
-	document.querySelector(".drum")[i].addEventListener("click", handleClick);
-
-
-	function handleClick() {
-		alert("I got clicked");
-	}
+	document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+		var audio = new Audio('sounds/tom-1.mp3');
+		audio.play();
+	});
 
 }
 
